fdd04eb46d836c2380b911dffa1cd6c6ad9b10f1
Unum Sarfraz <anamsarfraz@gmail.com>
2025-01-10 03:56:01 -0800

    Updating kb json and metadata

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # list of global constants
 
 KNOWLEDGE_BASE_PATH = "knowledge_base.json"
-demo_media_labels = {"LLM Agents Bootcamp"}
-immersive_demo_labels = {"LLM Agents Bootcamp"}
+demo_media_labels = {"CES 2025"}
+immersive_demo_labels = {"CES 2025"}
